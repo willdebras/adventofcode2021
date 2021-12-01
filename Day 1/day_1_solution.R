77bc3a51838c3291182a69ee6f77cf539235850d
willdebras <wtb3na@virginia.edu>
2021-11-30 23:30:10 -0600

    Day 1 of advent of code

---
+++ Day 1/day_1_solution.R
@@ -0,0 +1,37 @@
+# Day 1 
+
+input <- read.delim("Day 1/day_1_input.txt", header = F)
+
+# Part 1
+
+day1_part1 <- function(inputvec) {
+  
+  # Compute lagged differences
+  diffs <- diff(inputvec)
+  
+  # Return length of vector of positive lagged differences
+  return(length(diffs[diffs>0]))
+  
+}
+
+day1_part1(input$V1)
+
+
+
+# Part 2
+
+day1_part2 <- function(inputvec) {
+  
+  # Compute linear filtering (lagged sum of 3 values)
+  lagged_sums <- filter(inputvec, filter=c(1,1,1), sides=1)
+  
+  # Compute the lagged differences of these sums
+  diffs_sums <- diff(lagged_sums)
+  
+  # Return length of vector of positive lagged differences
+  return(length(diffs_sums[!is.na(diffs_sums) & diffs_sums>0]))
+}
+
+day1_part2(input$V1)
+
+
---
+++ README.md
@@ -0,0 +1,14 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+### adventofcode2021
+
+<!-- badges: start -->
+
+![](https://img.shields.io/badge/star%20count-NA-yellow.svg)
+<!-- badges: end -->
+
+If you are finding this, this is my personal repo for the 2021 Advent of
+Code challenge. I am using this to practice algorithmic thinking, branch
+out and write more code in Julia, and to just have fun. Feel free to
+ping me if you have questions!
